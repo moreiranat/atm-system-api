05e0b0c067b81467688c267ec3145e8ce8dabea3
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-25 16:56:18 -0300

    Project created and dependencies added

---
+++ src/test/java/com/api/atmsystem/AtmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.atmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
